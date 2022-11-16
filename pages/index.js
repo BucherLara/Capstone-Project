196be617cfe2504b784dc719e475bde36c3acb25
Lara Bucher <bucher.lara@web.de>
2022-11-16 16:12:25 +0100

    render facility names

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,27 @@
 import fetcher from "../helpers/fetcher";
 import useSWR from "swr";
+import styled from "styled-components";
+
 export default function Home() {
   const { data, error } = useSWR("api/facilities", fetcher);
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
-  console.log(data);
+
   return (
     <>
       <h1>Einrichtungsnamen</h1>
-      <p>{data[0].name}</p>
+      {data.map((facility) => {
+        return (
+          <ul key={facility.id}>
+            <ListItem>{facility.name}</ListItem>
+          </ul>
+        );
+      })}
     </>
   );
 }
+
+const ListItem = styled.li`
+  list-style: none;
+`;
