fde9cf50af7d6b45020ea75cf4e0c2e55741355b
Lara Bucher <bucher.lara@web.de>
2022-11-28 16:29:59 +0100

    add svg instead of img

--- components/Facility/index.js
+++ components/Facility/index.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Image from "next/image";
 import Link from "next/link";
 import { ListItem } from "../FacilityList";
+import { css } from "styled-components";
 
 export default function Facility({ facility, setFacilities }) {
   const { name, adress, target, requirements, image, link, tel, id } = facility;
@@ -38,7 +39,18 @@ export default function Facility({ facility, setFacilities }) {
               toggleBookmark(id);
             }}
           >
-            <Image
+            <StyledSvg
+              variant={
+                facility.isBookmarked ? "bookmarkActive" : "bookmarkNotActive"
+              }
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+            >
+              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+            </StyledSvg>
+            {/* <Image
               src={
                 facility.isBookmarked === false
                   ? "/icons/isNotBookmarked.png"
@@ -47,7 +59,7 @@ export default function Facility({ facility, setFacilities }) {
               alt="isNotBookmarkedHeart"
               width={40}
               height={40}
-            />
+            /> */}
           </StyledButton>
           <ListItem>
             <b>Kategorie: </b> {facility.category}
@@ -105,5 +117,22 @@ const StyledButton = styled.button`
   padding: 0;
   background-color: white;
 `;
+const StyledSvg = styled.svg`
+  width: 45px;
+  height: 45px;
+  margin-left: 150px;
+  ${({ variant }) =>
+    variant === "bookmarkActive" &&
+    css`
+      fill: #ff7f7f;
+    `}
 
+  ${({ variant }) =>
+    variant === "bookmarkNotActive" &&
+    css`
+      fill: transparent;
+      stroke: red;
+      color: red;
+    `}
+`;
 export { StyledList, StyledImage };
